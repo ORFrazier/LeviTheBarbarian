4267a9dea64a8e0653360c016956fb9bc381f256
ORFrazier <zores1991@live.com>
2022-09-20 14:43:28 -0400

    Create Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevitheBarbarian
+{
+    public class Weapon
+    {
+        public int weapon;
+        public string name;
+
+        public static void CreateWeapons()
+        {
+            Weapon a = new Weapon();
+            a.weapon = 1;
+            Console.WriteLine("Weapon: " + a.weapon);
+
+            Weapon b = new Weapon();
+            b.weapon = 2;
+            Console.WriteLine("Weapon: " + b.weapon);
+
+            Weapon c = new Weapon();
+            c.weapon = 3;
+            Console.WriteLine("Weapon: " + c.weapon);
+
+            if (a.weapon == 1)
+            {
+                Console.WriteLine(a.name);
+            }
+            if (b.weapon == 2)
+            {
+                Console.WriteLine(b.name);
+            }
+            if (c.weapon == 3)
+            {
+                Console.WriteLine(c.name);
+            }
+        }
+    }
+}
